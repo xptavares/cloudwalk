00c88e4bc789a46d712ac987e3b8af9dd006b002
Alexandre Tavares <xptavares@gmail.com>
2024-02-03 23:45:14 -0300

    create validate on device id and request

--- app/services/transaction/create_service.rb
+++ app/services/transaction/create_service.rb
@@ -16,14 +16,34 @@ class Transaction::CreateService
   def perform
     # sleep(1000.0/24.0)
     return { transaction_id: transaction_id, recommendation: 'disapprove' } unless validations
-    { transaction_id: transaction_id, recommendation: 'approve' }
+
+    transaction = Transaction.new(id: transaction_id, merchant: merchant, user: user, card: card, transaction_date: transaction_date, transaction_amount: transaction_amount, device_id: device_id)
+    return { transaction_id: transaction_id, recommendation: 'disapprove' } unless transaction.valid?
+
+    transaction.save
+
+    { transaction_id: transaction.id.to_s, recommendation: 'approve' }
   end
 
   private
-  def validations
-    return false unless Transaction::ValidateAmountService.new(user_id, transaction_date, transaction_amount).perform
-    return false unless Transaction::ValidateChargebackService.new(user_id).perform
-    true
-  end
+    def validations
+      return false unless Transaction::ValidateAmountService.new(user_id, transaction_date, transaction_amount).perform
+      return false unless Transaction::ValidateChargebackService.new(user_id).perform
+      return false unless device_id
+      true
+    end
+
+    def user
+      @user ||= User.where(id: user_id).first_or_create
+    end
+
+    def merchant
+      @merchant ||= Merchant.where(id: merchant_id).first_or_create
+    end
+
+    def card
+      @card ||= Card.where(card_number: card_number, user_id: user_id).first_or_create
+    end
+    
   
 end
--- spec/requests/transaction_spec.rb
+++ spec/requests/transaction_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Transactions", type: :request do
       merchant_id: 29744,
       user_id: 97051,
       card_number: "434505******9116",
-      transaction_date: "2019-11-31T23:16:32.812632",
+      transaction_date: Time.zone.now,
       transaction_amount: 999,
       device_id: 285475
     }
@@ -24,7 +24,7 @@ RSpec.describe "Transactions", type: :request do
       expect(json[:recommendation]).to  eq('approve')
     end
 
-    it "should raise error if perform twice" do
+    it "should disapprove with no limit" do
       post "/api/transactions", params: params
       expect(response).to have_http_status(:success)
 
@@ -34,7 +34,7 @@ RSpec.describe "Transactions", type: :request do
       expect(json[:recommendation]).to  eq('approve')
 
       params["transaction_id"] = 2342358
-      params["transaction_date"] = '2019-12-02T23:16:32.812632'
+      params["transaction_date"] = Time.zone.now
 
       post "/api/transactions", params: params
       expect(response).to have_http_status(:success)
@@ -44,6 +44,16 @@ RSpec.describe "Transactions", type: :request do
       expect(json[:transaction_id]).to  eq('2342358')
       expect(json[:recommendation]).to  eq('disapprove')
     end
-  end
 
+    it "should disapprove without device_id" do
+      params.delete(:device_id)
+      post "/api/transactions", params: params
+      expect(response).to have_http_status(:success)
+
+      json = JSON.parse(response.body).deep_symbolize_keys
+
+      expect(json[:transaction_id]).to  eq('2342357')
+      expect(json[:recommendation]).to  eq('disapprove')
+    end
+  end
 end
